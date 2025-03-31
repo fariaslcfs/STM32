d37589b1f3c610f5da101a1d8d23afba8d073311
farialcfs <fariaslcfs@gmail.com>
2025-03-31 15:27:31 -0300

    Adicionado um watchdog IWDG (independent watchdog) ao projeto ESP301_Bluetooth_Bridge_USART1_2. Necessidade de configurar o aquivo stm32f4xx_hal_conf.h retirando o caracter de comentário: [200~/* #define HAL_TIM_MODULE_ENABLED */

--- ESP301_Bluetooth_Bridge_USART1_2/Core/Src/main.c
+++ ESP301_Bluetooth_Bridge_USART1_2/Core/Src/main.c
@@ -19,7 +19,6 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -43,6 +42,9 @@
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
+IWDG_HandleTypeDef hiwdg; /* Adicionado para o Watchdog */
+
+
 
 /* USER CODE BEGIN PV */
 uint8_t usart1_rx_buffer[1]; // Buffer maior para USART1 (Bluetooth)
@@ -54,6 +56,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_IWDG_Init(void); /* Adicionado para o Watchdog */
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -93,6 +96,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
+  MX_IWDG_Init(); /* Inicializa o Watchdog */
   /* USER CODE BEGIN 2 */
   // Configura prioridades das interrupções no NVIC para máxima responsividade
   HAL_NVIC_SetPriority(USART1_IRQn, 0, 0); // Prioridade máxima para USART1
@@ -118,6 +122,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+    HAL_IWDG_Refresh(&hiwdg); // Alimenta o Watchdog para evitar reset
   }
   /* USER CODE END 3 */
 }
@@ -162,6 +167,22 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief IWDG Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_IWDG_Init(void)
+{
+  hiwdg.Instance = IWDG;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_32; // Exemplo: ~1 kHz
+  hiwdg.Init.Reload = 1000; // Exemplo: ~1 segundo até reset
+  if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     Error_Handler();
   }
@@ -326,6 +347,18 @@ void assert_failed(uint8_t *file, uint32_t line)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 ///* USER CODE BEGIN Header */
 ///**
 //  ******************************************************************************
@@ -373,8 +406,8 @@ void assert_failed(uint8_t *file, uint32_t line)
 //UART_HandleTypeDef huart2;
 //
 ///* USER CODE BEGIN PV */
-//uint8_t usart1_rx_buffer[1]; // Buffer para receber da USART1 (Bluetooth)
-//uint8_t usart2_rx_buffer[1]; // Buffer para receber da USART2 (ESP301)
+//uint8_t usart1_rx_buffer[1]; // Buffer maior para USART1 (Bluetooth)
+//uint8_t usart2_rx_buffer[1]; // Buffer maior para USART2 (ESP301)
 ///* USER CODE END PV */
 //
 ///* Private function prototypes -----------------------------------------------*/
@@ -423,10 +456,10 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  MX_USART2_UART_Init();
 //  /* USER CODE BEGIN 2 */
 //  // Configura prioridades das interrupções no NVIC para máxima responsividade
-////  HAL_NVIC_SetPriority(USART1_IRQn, 0, 0); // Prioridade máxima para USART1
-////  HAL_NVIC_SetPriority(USART2_IRQn, 0, 0); // Prioridade máxima para USART2
-////  HAL_NVIC_EnableIRQ(USART1_IRQn);
-////  HAL_NVIC_EnableIRQ(USART2_IRQn);
+//  HAL_NVIC_SetPriority(USART1_IRQn, 0, 0); // Prioridade máxima para USART1
+//  HAL_NVIC_SetPriority(USART2_IRQn, 0, 1); // Prioridade ligeiramente menor para USART2
+//  HAL_NVIC_EnableIRQ(USART1_IRQn);
+//  HAL_NVIC_EnableIRQ(USART2_IRQn);
 //
 //  // Piscar LED para indicar inicialização
 //  for (int i = 0; i < 8; i++) {
@@ -502,13 +535,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  */
 //static void MX_USART1_UART_Init(void)
 //{
-//  /* USER CODE BEGIN USART1_Init 0 */
-//
-//  /* USER CODE END USART1_Init 0 */
-//
-//  /* USER CODE BEGIN USART1_Init 1 */
-//
-//  /* USER CODE END USART1_Init 1 */
 //  huart1.Instance = USART1;
 //  huart1.Init.BaudRate = 19200;
 //  huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -521,9 +547,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  {
 //    Error_Handler();
 //  }
-//  /* USER CODE BEGIN USART1_Init 2 */
-//
-//  /* USER CODE END USART1_Init 2 */
 //}
 //
 ///**
@@ -533,13 +556,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  */
 //static void MX_USART2_UART_Init(void)
 //{
-//  /* USER CODE BEGIN USART2_Init 0 */
-//
-//  /* USER CODE END USART2_Init 0 */
-//
-//  /* USER CODE BEGIN USART2_Init 1 */
-//
-//  /* USER CODE END USART2_Init 1 */
 //  huart2.Instance = USART2;
 //  huart2.Init.BaudRate = 19200;
 //  huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -552,9 +568,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  {
 //    Error_Handler();
 //  }
-//  /* USER CODE BEGIN USART2_Init 2 */
-//
-//  /* USER CODE END USART2_Init 2 */
 //}
 //
 ///**
@@ -565,8 +578,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 //static void MX_GPIO_Init(void)
 //{
 //  GPIO_InitTypeDef GPIO_InitStruct = {0};
-//  /* USER CODE BEGIN MX_GPIO_Init_1 */
-//  /* USER CODE END MX_GPIO_Init_1 */
 //
 //  /* GPIO Ports Clock Enable */
 //  __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -582,9 +593,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  GPIO_InitStruct.Pull = GPIO_NOPULL;
 //  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 //  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-//
-//  /* USER CODE BEGIN MX_GPIO_Init_2 */
-//  /* USER CODE END MX_GPIO_Init_2 */
 //}
 //
 ///* USER CODE BEGIN 4 */
@@ -596,13 +604,44 @@ void assert_failed(uint8_t *file, uint32_t line)
 //void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 //{
 //  if (huart->Instance == USART1) {
-//    // Recebeu do Bluetooth, retransmite para ESP301
-//    HAL_UART_Transmit(&huart2, usart1_rx_buffer, 1, 5);
+//    // Recebeu do Bluetooth, retransmite para ESP301 usando interrupção
+//    HAL_UART_Transmit_IT(&huart2, usart1_rx_buffer, 1);
+//  }
+//  else if (huart->Instance == USART2) {
+//    // Recebeu do ESP301, retransmite para Bluetooth usando interrupção
+//    HAL_UART_Transmit_IT(&huart1, usart2_rx_buffer, 1);
+//  }
+//}
+//
+///**
+//  * @brief  Callback chamado quando a transmissão por interrupção é concluída
+//  * @param  huart: Ponteiro para a estrutura UART
+//  * @retval None
+//  */
+//void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+//{
+//  if (huart->Instance == USART1) {
+//    // Transmissão para Bluetooth concluída, reinicia recepção no ESP301
+//    HAL_UART_Receive_IT(&huart2, usart2_rx_buffer, 1);
+//  }
+//  else if (huart->Instance == USART2) {
+//    // Transmissão para ESP301 concluída, reinicia recepção no Bluetooth
+//    HAL_UART_Receive_IT(&huart1, usart1_rx_buffer, 1);
+//  }
+//}
+//
+///**
+//  * @brief  Callback chamado em caso de erro na UART
+//  * @param  huart: Ponteiro para a estrutura UART
+//  * @retval None
+//  */
+//void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+//{
+//  // Reinicia recepção em caso de erro
+//  if (huart->Instance == USART1) {
 //    HAL_UART_Receive_IT(&huart1, usart1_rx_buffer, 1);
 //  }
 //  else if (huart->Instance == USART2) {
-//    // Recebeu do ESP301, retransmite para Bluetooth
-//    HAL_UART_Transmit(&huart1, usart2_rx_buffer, 1, 5);
 //    HAL_UART_Receive_IT(&huart2, usart2_rx_buffer, 1);
 //  }
 //}
@@ -614,13 +653,10 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  */
 //void Error_Handler(void)
 //{
-//  /* USER CODE BEGIN Error_Handler_Debug */
-//  /* User can add his own implementation to report the HAL error return state */
 //  __disable_irq();
 //  while (1)
 //  {
 //  }
-//  /* USER CODE END Error_Handler_Debug */
 //}
 //
 //#ifdef  USE_FULL_ASSERT
@@ -633,21 +669,346 @@ void assert_failed(uint8_t *file, uint32_t line)
 //  */
 //void assert_failed(uint8_t *file, uint32_t line)
 //{
-//  /* USER CODE BEGIN 6 */
-//  /* User can add his own implementation to report the file name and line number,
-//     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-//  /* USER CODE END 6 */
 //}
 //#endif /* USE_FULL_ASSERT */
-
-
-
-
-
-
-
-
-
-
-
-
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/////* USER CODE BEGIN Header */
+/////**
+////  ******************************************************************************
+////  * @file           : main.c
+////  * @brief          : Main program body
+////  ******************************************************************************
+////  * @attention
+////  *
+////  * Copyright (c) 2025 STMicroelectronics.
+////  * All rights reserved.
+////  *
+////  * This software is licensed under terms that can be found in the LICENSE file
+////  * in the root directory of this software component.
+////  * If no LICENSE file comes with this software, it is provided AS-IS.
+////  *
+////  ******************************************************************************
+////  */
+/////* USER CODE END Header */
+////
+/////* Includes ------------------------------------------------------------------*/
+////#include "main.h"
+////
+/////* Private includes ----------------------------------------------------------*/
+/////* USER CODE BEGIN Includes */
+////
+/////* USER CODE END Includes */
+////
+/////* Private typedef -----------------------------------------------------------*/
+/////* USER CODE BEGIN PTD */
+////
+/////* USER CODE END PTD */
+////
+/////* Private define ------------------------------------------------------------*/
+/////* USER CODE BEGIN PD */
+////
+/////* USER CODE END PD */
+////
+/////* Private macro -------------------------------------------------------------*/
+/////* USER CODE BEGIN PM */
+////
+/////* USER CODE END PM */
+////
+/////* Private variables ---------------------------------------------------------*/
+////UART_HandleTypeDef huart1;
+////UART_HandleTypeDef huart2;
+////
+/////* USER CODE BEGIN PV */
+////uint8_t usart1_rx_buffer[1]; // Buffer para receber da USART1 (Bluetooth)
+////uint8_t usart2_rx_buffer[1]; // Buffer para receber da USART2 (ESP301)
+/////* USER CODE END PV */
+////
+/////* Private function prototypes -----------------------------------------------*/
+////void SystemClock_Config(void);
+////static void MX_GPIO_Init(void);
+////static void MX_USART1_UART_Init(void);
+////static void MX_USART2_UART_Init(void);
+/////* USER CODE BEGIN PFP */
+////
+/////* USER CODE END PFP */
+////
+/////* Private user code ---------------------------------------------------------*/
+/////* USER CODE BEGIN 0 */
+////
+/////* USER CODE END 0 */
+////
+/////**
+////  * @brief  The application entry point.
+////  * @retval int
+////  */
+////int main(void)
+////{
+////  /* USER CODE BEGIN 1 */
+////
+////  /* USER CODE END 1 */
+////
+////  /* MCU Configuration--------------------------------------------------------*/
+////
+////  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+////  HAL_Init();
+////
+////  /* USER CODE BEGIN Init */
+////
+////  /* USER CODE END Init */
+////
+////  /* Configure the system clock */
+////  SystemClock_Config();
+////
+////  /* USER CODE BEGIN SysInit */
+////
+////  /* USER CODE END SysInit */
+////
+////  /* Initialize all configured peripherals */
+////  MX_GPIO_Init();
+////  MX_USART1_UART_Init();
+////  MX_USART2_UART_Init();
+////  /* USER CODE BEGIN 2 */
+////  // Configura prioridades das interrupções no NVIC para máxima responsividade
+//////  HAL_NVIC_SetPriority(USART1_IRQn, 0, 0); // Prioridade máxima para USART1
+//////  HAL_NVIC_SetPriority(USART2_IRQn, 0, 0); // Prioridade máxima para USART2
+//////  HAL_NVIC_EnableIRQ(USART1_IRQn);
+//////  HAL_NVIC_EnableIRQ(USART2_IRQn);
+////
+////  // Piscar LED para indicar inicialização
+////  for (int i = 0; i < 8; i++) {
+////    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+////    HAL_Delay(100);
+////  }
+////
+////  // Inicia recepção com interrupção em ambas as UARTs
+////  HAL_UART_Receive_IT(&huart1, usart1_rx_buffer, 1); // USART1 (Bluetooth)
+////  HAL_UART_Receive_IT(&huart2, usart2_rx_buffer, 1); // USART2 (ESP301)
+////  /* USER CODE END 2 */
+////
+////  /* Infinite loop */
+////  /* USER CODE BEGIN WHILE */
+////  while (1)
+////  {
+////    /* USER CODE END WHILE */
+////
+////    /* USER CODE BEGIN 3 */
+////  }
+////  /* USER CODE END 3 */
+////}
+////
+/////**
+////  * @brief System Clock Configuration
+////  * @retval None
+////  */
+////void SystemClock_Config(void)
+////{
+////  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+////  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+////
+////  /** Configure the main internal regulator output voltage
+////  */
+////  __HAL_RCC_PWR_CLK_ENABLE();
+////  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+////
+////  /** Initializes the RCC Oscillators according to the specified parameters
+////  * in the RCC_OscInitTypeDef structure.
+////  */
+////  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+////  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+////  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+////  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+////  RCC_OscInitStruct.PLL.PLLM = 12;
+////  RCC_OscInitStruct.PLL.PLLN = 96;
+////  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+////  RCC_OscInitStruct.PLL.PLLQ = 4;
+////  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+////  {
+////    Error_Handler();
+////  }
+////
+////  /** Initializes the CPU, AHB and APB buses clocks
+////  */
+////  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+////                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+////  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+////  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+////  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+////  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+////
+////  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+////  {
+////    Error_Handler();
+////  }
+////}
+////
+/////**
+////  * @brief USART1 Initialization Function
+////  * @param None
+////  * @retval None
+////  */
+////static void MX_USART1_UART_Init(void)
+////{
+////  /* USER CODE BEGIN USART1_Init 0 */
+////
+////  /* USER CODE END USART1_Init 0 */
+////
+////  /* USER CODE BEGIN USART1_Init 1 */
+////
+////  /* USER CODE END USART1_Init 1 */
+////  huart1.Instance = USART1;
+////  huart1.Init.BaudRate = 19200;
+////  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+////  huart1.Init.StopBits = UART_STOPBITS_1;
+////  huart1.Init.Parity = UART_PARITY_NONE;
+////  huart1.Init.Mode = UART_MODE_TX_RX;
+////  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+////  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+////  if (HAL_UART_Init(&huart1) != HAL_OK)
+////  {
+////    Error_Handler();
+////  }
+////  /* USER CODE BEGIN USART1_Init 2 */
+////
+////  /* USER CODE END USART1_Init 2 */
+////}
+////
+/////**
+////  * @brief USART2 Initialization Function
+////  * @param None
+////  * @retval None
+////  */
+////static void MX_USART2_UART_Init(void)
+////{
+////  /* USER CODE BEGIN USART2_Init 0 */
+////
+////  /* USER CODE END USART2_Init 0 */
+////
+////  /* USER CODE BEGIN USART2_Init 1 */
+////
+////  /* USER CODE END USART2_Init 1 */
+////  huart2.Instance = USART2;
+////  huart2.Init.BaudRate = 19200;
+////  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+////  huart2.Init.StopBits = UART_STOPBITS_1;
+////  huart2.Init.Parity = UART_PARITY_NONE;
+////  huart2.Init.Mode = UART_MODE_TX_RX;
+////  huart2.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
+////  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+////  if (HAL_UART_Init(&huart2) != HAL_OK)
+////  {
+////    Error_Handler();
+////  }
+////  /* USER CODE BEGIN USART2_Init 2 */
+////
+////  /* USER CODE END USART2_Init 2 */
+////}
+////
+/////**
+////  * @brief GPIO Initialization Function
+////  * @param None
+////  * @retval None
+////  */
+////static void MX_GPIO_Init(void)
+////{
+////  GPIO_InitTypeDef GPIO_InitStruct = {0};
+////  /* USER CODE BEGIN MX_GPIO_Init_1 */
+////  /* USER CODE END MX_GPIO_Init_1 */
+////
+////  /* GPIO Ports Clock Enable */
+////  __HAL_RCC_GPIOC_CLK_ENABLE();
+////  __HAL_RCC_GPIOH_CLK_ENABLE();
+////  __HAL_RCC_GPIOA_CLK_ENABLE();
+////
+////  /*Configure GPIO pin Output Level */
+////  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+////
+////  /*Configure GPIO pin : PC13 */
+////  GPIO_InitStruct.Pin = GPIO_PIN_13;
+////  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+////  GPIO_InitStruct.Pull = GPIO_NOPULL;
+////  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+////  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+////
+////  /* USER CODE BEGIN MX_GPIO_Init_2 */
+////  /* USER CODE END MX_GPIO_Init_2 */
+////}
+////
+/////* USER CODE BEGIN 4 */
+/////**
+////  * @brief  Callback chamado quando um dado é recebido por interrupção
+////  * @param  huart: Ponteiro para a estrutura UART
+////  * @retval None
+////  */
+////void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+////{
+////  if (huart->Instance == USART1) {
+////    // Recebeu do Bluetooth, retransmite para ESP301
+////    HAL_UART_Transmit(&huart2, usart1_rx_buffer, 1, 5);
+////    HAL_UART_Receive_IT(&huart1, usart1_rx_buffer, 1);
+////  }
+////  else if (huart->Instance == USART2) {
+////    // Recebeu do ESP301, retransmite para Bluetooth
+////    HAL_UART_Transmit(&huart1, usart2_rx_buffer, 1, 5);
+////    HAL_UART_Receive_IT(&huart2, usart2_rx_buffer, 1);
+////  }
+////}
+/////* USER CODE END 4 */
+////
+/////**
+////  * @brief  This function is executed in case of error occurrence.
+////  * @retval None
+////  */
+////void Error_Handler(void)
+////{
+////  /* USER CODE BEGIN Error_Handler_Debug */
+////  /* User can add his own implementation to report the HAL error return state */
+////  __disable_irq();
+////  while (1)
+////  {
+////  }
+////  /* USER CODE END Error_Handler_Debug */
+////}
+////
+////#ifdef  USE_FULL_ASSERT
+/////**
+////  * @brief  Reports the name of the source file and the source line number
+////  *         where the assert_param error has occurred.
+////  * @param  file: pointer to the source file name
+////  * @param  line: assert_param error line source number
+////  * @retval None
+////  */
+////void assert_failed(uint8_t *file, uint32_t line)
+////{
+////  /* USER CODE BEGIN 6 */
+////  /* User can add his own implementation to report the file name and line number,
+////     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+////  /* USER CODE END 6 */
+////}
+////#endif /* USE_FULL_ASSERT */
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
